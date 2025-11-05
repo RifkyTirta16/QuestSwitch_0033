ad07586c42c3f59479ec265b906c62438a476418
RifkyTirta16 <rifkyz949@gmail.com>
2025-11-05 17:58:19 +0700

    Perubahan tipe data

--- Quest.java
+++ Quest.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 
 public class Quest {
     public static void main(String[] args) {
-        Scanner ns = new Scanner(System.in);
-        System.out.println("Masukkan Nilai Struktur Data: ");
-        int nilai = ns.nextInt();
-        System.out.println("Masukkan Nilai Pemograman Berorientasi Objek: ");
-        int nilai2 = ns.nextInt();
-        System.out.println("Masukkan Nilai Basis Data: ");
-        int nilai3 = ns.nextInt();
-        System.out.println("Masukkan Nilai Algoritma dan Pemograman: ");
-        int nilai4 = ns.nextInt();
-        System.out.println("Masukkan Nilai Rekaya Perangkat Lunak: ");
-        int nilai5 = ns.nextInt();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai Struktur Data: ");
+        double strukturData = input.nextDouble();
 
+        System.out.print("Masukkan nilai Pemrograman Berorientasi Objek: ");
+        double pbo = input.nextDouble();
+
+        System.out.print("Masukkan nilai Basis Data: ");
+        double basisData = input.nextDouble();
+
+        System.out.print("Masukkan nilai Algoritma dan Pemrograman: ");
+        double algo = input.nextDouble();
+
+        System.out.print("Masukkan nilai Rekayasa Perangkat Lunak: ");
+        double rpl = input.nextDouble();
     }
 }

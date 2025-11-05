0fee8c3bb06a9f62702e0b4b0af92c956fe24760
RifkyTirta16 <rifkyz949@gmail.com>
2025-11-05 17:51:47 +0700

    Menambahkan code untuk input nilai setiap mata kuliah

--- Quest.java
+++ Quest.java
@@ -6,7 +6,14 @@ public class Quest {
         Scanner ns = new Scanner(System.in);
         System.out.println("Masukkan Nilai Struktur Data: ");
         int nilai = ns.nextInt();
-        
+        System.out.println("Masukkan Nilai Pemograman Berorientasi Objek: ");
+        int nilai2 = ns.nextInt();
+        System.out.println("Masukkan Nilai Basis Data: ");
+        int nilai3 = ns.nextInt();
+        System.out.println("Masukkan Nilai Algoritma dan Pemograman: ");
+        int nilai4 = ns.nextInt();
+        System.out.println("Masukkan Nilai Rekaya Perangkat Lunak: ");
+        int nilai5 = ns.nextInt();
 
     }
 }

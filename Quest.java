429c52e2ef65bc08b44178135258f075a0568878
RifkyTirta16 <rifkyz949@gmail.com>
2025-11-05 20:04:31 +0700

    Membuat predikat IPK

--- Quest.java
+++ Quest.java
@@ -23,7 +23,20 @@ public class Quest {
         double ratarata = total / 5;
         double ipk = ratarata / 100 * 4.0;
 
-        
-
+        String predikat;
+        if (ipk >= 3.75){
+            predikat = "A (Sangat Baik)";
+        }else if (ipk >= 3.5){
+            predikat = "AB (Baik)";
+        }else if (ipk >= 3.0){
+            predikat = "B (Cukup Baik)";
+        }else if (ipk >= 2.5){
+            predikat = "BC (Kurang Baik)";
+        }else{
+            predikat = "C (Buruk)";
+        }
+        System.out.println("Nilai Rata-rata: " + ratarata);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Predikat: " + predikat);
     }
 }

99ac35c0739a6ea23a9388262da983f0be332868
RifkyTirta16 <rifkyz949@gmail.com>
2025-11-05 17:44:45 +0700

    Menambahkan scanner

--- Quest.java
+++ Quest.java
@@ -1,3 +1,12 @@
-public static void main(String[] args) {
-   
+
+import java.util.Scanner;
+
+public class Quest {
+    public static void main(String[] args) {
+        Scanner ns = new Scanner(System.in);
+        System.out.println("Masukkan Nilai Struktur Data: ");
+        int nilai = ns.nextInt();
+        
+
+    }
 }
